7ce9a8c8546854678c93883299d789987a2c7466
Christopher Leal <chris_t_o@hotmail.com>
2021-04-08 01:11:52 -0500

    home cubit modified

--- lib/presentation/home/home_cubit.dart
+++ lib/presentation/home/home_cubit.dart
@@ -1,25 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:palette_generator/palette_generator.dart';
 
-import '../../domain/entities/pokemon_list_item.dart';
-import '../../domain/usecases/pokemon_list_use_case.dart';
+class HomeCubit extends Cubit<bool> {
+  HomeCubit() : super(false);
 
-class HomeCubit extends Cubit<List<PokemonListItem>> {
-  HomeCubit(this._pokemonListUseCase) : super([]);
-
-  final PokemonListUseCase _pokemonListUseCase;
-
-  Future<void> init() async {
-    emit([...state]);
-    final pokemons = await _pokemonListUseCase.getPokemons();
-    emit([...state, ...pokemons]);
-  }
-
-  void getBackgroundColor(int index, ImageProvider imageProvider) {
-    PaletteGenerator.fromImageProvider(imageProvider).then((generator) {
-      state[index].backgroundColor = generator.dominantColor.color ?? Colors.red;
-      emit([...state]);
-    });
+  void changeView() {
+    emit(!state);
   }
 }
