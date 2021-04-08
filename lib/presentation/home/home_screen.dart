bb904e6a42d9d650cd479774941bcd2fd9beacdb
Christopher Leal <chris_t_o@hotmail.com>
2021-04-08 01:12:12 -0500

    home scrren updated

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,98 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokedex_app/presentation/pokemons/pokemon_grid_screen.dart';
+import 'package:pokedex_app/presentation/pokemons/pokemon_list_screen.dart';
+import 'package:pokedex_app/presentation/pokemons/pokemons_cubit.dart';
 
-import '../../domain/entities/pokemon_list_item.dart';
 import 'home_cubit.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen._();
-
-  static Widget init() {
-    return BlocProvider(
-      create: (context) => HomeCubit(context.read())..init(),
-      child: const HomeScreen._(),
-    );
-  }
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final _scrollController = ScrollController();
-  @override
-  void initState() {
-    _scrollController.addListener(() {
-      if (_scrollController.offset == _scrollController.position.maxScrollExtent) {
-        context.read<HomeCubit>().init();
-      }
-    });
-
-    super.initState();
+class HomeScreen extends StatelessWidget {
+  Widget _getBodyWidgets(BuildContext context) {
+    return context.read<HomeCubit>().state ? PokemonGridScreen() : PokemonListScreen();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => PokemonsCubit(context.read())..getPokemons()),
+        BlocProvider(create: (context) => HomeCubit()),
+      ],
+      child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: const Text('Pokedex'),
-        ),
-        body: BlocBuilder<HomeCubit, List<PokemonListItem>>(
-          builder: (context, state) {
-            if (state.isEmpty) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            return ListView.builder(
-              controller: _scrollController,
-              physics: const BouncingScrollPhysics(),
-              itemCount: state.length + 1,
-              itemBuilder: (context, index) {
-                if (index >= state.length) {
-                  return const Center(
-                      child: CupertinoActivityIndicator(
-                    radius: 20,
-                  ));
-                }
-                final pokemon = state[index];
-                // context.read<HomeCubit>().getBackgroundColor(index, NetworkImage(pokemon.picture));
-                return _PokemonItem(pokemon: pokemon);
+          actions: [
+            BlocBuilder<HomeCubit, bool>(
+              builder: (context, state) {
+                return IconButton(
+                  icon: Icon(state ? Icons.grid_view : Icons.list),
+                  onPressed: () {
+                    context.read<HomeCubit>().changeView();
+                  },
+                );
               },
+            )
+          ],
+        ),
+        body: BlocBuilder<HomeCubit, bool>(
+          builder: (context, state) {
+            return AnimatedSwitcher(
+              duration: const Duration(milliseconds: 500),
+              child: _getBodyWidgets(context),
             );
           },
-        ));
-  }
-}
-
-class _PokemonItem extends StatelessWidget {
-  const _PokemonItem({
-    Key key,
-    @required this.pokemon,
-  }) : super(key: key);
-
-  final PokemonListItem pokemon;
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    return Stack(
-      children: [
-        Card(
-          color: pokemon.backgroundColor,
-          child: Image.asset(
-            'assets/pokeball.png',
-          ),
         ),
-        Center(
-          child: Image.network(
-            pokemon.picture,
-            fit: BoxFit.contain,
-            width: size.width * 0.5,
-            height: size.width * 0.5,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
